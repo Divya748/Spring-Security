86e9b2257f742370a76f0eb69b1677b296eec91d
Divya748 <divyaakshay011@gmail.com>
2022-07-14 16:26:15 +0530

    commit

--- spring-security/src/main/java/com/springsecurity/controller/SecurityController.java
+++ spring-security/src/main/java/com/springsecurity/controller/SecurityController.java
@@ -3,6 +3,7 @@ package com.springsecurity.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//
 @RestController
 public class SecurityController {
 	
