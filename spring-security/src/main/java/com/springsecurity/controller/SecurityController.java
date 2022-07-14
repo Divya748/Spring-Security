a03829aceb06f45bac72330b55cd19d3e2053e25
Divya748 <divyaakshay011@gmail.com>
2022-07-14 16:24:58 +0530

    files added

---
+++ spring-security/src/main/java/com/springsecurity/controller/SecurityController.java
@@ -0,0 +1,25 @@
+package com.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+	
+	
+	@GetMapping("/")
+	public String homePage() {
+		return "home-page";
+	}
+	
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "admin-page";
+	}
+	
+	@GetMapping("/User")
+	public String userPage() {
+		return "home-page";
+	}
+
+}
